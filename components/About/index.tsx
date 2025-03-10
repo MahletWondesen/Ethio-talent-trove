64217bbba6dfc096c7e4b176a891a712529253b8
unknown <mahlet.wondesen@walletbirr.com>
2025-03-10 10:59:15 +0300

    first commit

--- components/About/index.tsx
+++ components/About/index.tsx
@@ -253,14 +253,14 @@ const About = () => {
               className="animate_right md:w-1/2"
             >
               <span className="font-medium uppercase text-black dark:text-white">
-                <span className="mb-4 mr-4 inline-flex rounded-full bg-meta px-4.5 py-1 uppercase text-white">
+                <span className="mb-4 mr-4 inline-flex rounded-full  bg-blue-300 px-4.5 py-1 uppercase text-white">
                   New
                 </span>{" "}
                 Ethiopia's Leading Job & Talent Marketplace
               </span>
               <h2 className="relative mb-6 text-3xl font-bold text-black dark:text-white xl:text-hero">
                 Connecting Professionals & Employers in{" "}
-                <span className="relative inline-block before:absolute before:bottom-2.5 before:left-0 before:-z-1 before:h-3 before:w-full before:bg-titlebg dark:before:bg-titlebgdark">
+                <span className="relative inline-block before:absolute before:bottom-2.5 before:left-0 before:-z-1 before:h-3 before:w-full before:bg-blue-200 dark:before:bg-titlebgdark">
                   Ethiopia
                 </span>
               </h2>
@@ -330,7 +330,7 @@ const About = () => {
               </h4>
               <h2 className="relative mb-6 text-3xl font-bold text-black dark:text-white xl:text-hero">
                 Designed for{" "}
-                <span className="relative inline-block before:absolute before:bottom-2.5 before:left-0 before:-z-1 before:h-3 before:w-full before:bg-titlebg2 dark:before:bg-titlebgdark">
+                <span className="relative inline-block before:absolute before:bottom-2.5 before:left-0 before:-z-1 before:h-3 before:w-full before:bg-blue-200 dark:before:bg-titlebgdark">
                   Ethiopia’s Workforce
                 </span>
               </h2>
