1fd4b2c414c97729ee9ac5443cbed7eac50e55c6
unknown <mahlet.wondesen@walletbirr.com>
2025-03-13 10:43:43 +0300

    first commit

--- components/Header/menuData.tsx
+++ components/Header/menuData.tsx
@@ -13,73 +13,23 @@ const menuData: Menu[] = [
     newTab: false,
     path: "/service",
   },
-  {
-    id: 2.1,
-    title: "Our News",
-    newTab: false,
-    path: "/blog",
-  },
-  {
-    id: 2.3,
-    title: "About Us",
-    newTab: false,
-    path: "/about",
-  },
+  // {
+  //   id: 2.1,
+  //   title: "Our News",
+  //   newTab: false,
+  //   path: "/blog",
+  // },
+  // {
+  //   id: 2.3,
+  //   title: "About Us",
+  //   newTab: false,
+  //   path: "/about",
+  // },
   {
     id: 2.4,
     title: "Careers",
     newTab: false,
     path: "/Careers",
-  },
-  // {
-  //   id: 3,
-  //   title: "About Us",
-  //   newTab: false,
-  //   submenu: [
-  //     {
-  //       id: 31,
-  //       title: "Blog Grid",
-  //       newTab: false,
-  //       path: "/blog",
-  //     },
-  //     {
-  //       id: 34,
-  //       title: "Sign In",
-  //       newTab: false,
-  //       path: "/auth/signin",
-  //     },
-  //     {
-  //       id: 35,
-  //       title: "Sign Up",
-  //       newTab: false,
-  //       path: "/auth/signup",
-  //     },
-  //     {
-  //       id: 35,
-  //       title: "Docs",
-  //       newTab: false,
-  //       path: "/docs",
-  //     },
-  //     {
-  //       id: 35.1,
-  //       title: "Support",
-  //       newTab: false,
-  //       path: "/support",
-  //     },
-  //     {
-  //       id: 36,
-  //       title: "404",
-  //       newTab: false,
-  //       path: "/error",
-  //     },
-  //   ],
-  // },
-
-  {
-    id: 4,
-    title: "Contact Us",
-    newTab: false,
-    path: "/support",
   },
   {
     id: 5,
@@ -87,6 +37,38 @@ const menuData: Menu[] = [
     newTab: false,
     path: "/faq",
   },
+  {
+    id: 3,
+    title: "About Us",
+    newTab: false,
+    submenu: [
+      {
+        id: 32,
+        title: "About Us",
+        newTab: false,
+        path: "/about",
+      },
+      {
+        id: 31,
+        title: "Our News",
+        newTab: false,
+        path: "/blog",
+      },
+      {
+        id: 33,
+        title: "Contact Us",
+        newTab: false,
+        path: "/support",
+      },
+    ],
+  },
+
+  // {
+  //   id: 4,
+  //   title: "Contact Us",
+  //   newTab: false,
+  //   path: "/support",
+  // },
 ];
 
 export default menuData;
