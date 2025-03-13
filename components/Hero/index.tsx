64aa4887369ef4326f17d8ea3d1d07c608fd395d
unknown <mahlet.wondesen@walletbirr.com>
2025-03-13 11:37:37 +0300

    first commit

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -20,7 +20,7 @@ const Hero = () => {
         <div className=" px-4 text-black  dark:text-black">
           <h1 className="mb-5 text-4xl font-extrabold tracking-tight drop-shadow-lg sm:text-5xl lg:text-6xl">
             Welcome to Ethio Talent{" "}
-            <span className="relative inline-block before:absolute before:bottom-2.5 before:left-0 before:-z-1   before:h-3 before:w-full before:bg-blue-200 dark:before:bg-titlebgdark">
+            <span className="relative inline-block before:absolute before:bottom-2.5 before:left-0 before:-z-1   before:h-3 before:w-full before:bg-blue-100 dark:before:bg-titlebgdark">
               Trove
             </span>
           </h1>
