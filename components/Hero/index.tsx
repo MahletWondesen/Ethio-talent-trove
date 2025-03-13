fe86af9beab1421a5a4f16c6a8c8cd108306903b
unknown <mahlet.wondesen@walletbirr.com>
2025-03-13 11:35:57 +0300

    first commit

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -17,14 +17,14 @@ const Hero = () => {
 
       {/* Hero Content */}
       <div className="flex h-full items-center justify-center text-center">
-        <div className="px-4 text-white dark:text-white">
+        <div className=" px-4 text-black  dark:text-black">
           <h1 className="mb-5 text-4xl font-extrabold tracking-tight drop-shadow-lg sm:text-5xl lg:text-6xl">
             Welcome to Ethio Talent{" "}
             <span className="relative inline-block before:absolute before:bottom-2.5 before:left-0 before:-z-1   before:h-3 before:w-full before:bg-blue-200 dark:before:bg-titlebgdark">
               Trove
             </span>
           </h1>
-          <p className="mx-auto max-w-2xl text-lg leading-relaxed drop-shadow-md">
+          <p className="dark:text-white!important mx-auto max-w-2xl text-lg leading-relaxed text-black  drop-shadow-md">
             At Ethio Talent Trove, we connect skilled professionals with
             outstanding opportunities. Based in Addis Ababa, Ethiopia, our
             mission is to bridge the gap between talent and organizations in
